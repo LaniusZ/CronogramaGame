0d08c8a5c804be7965ded4d46000416ce867223e
Alfonso Alamos <alfonso.informatico@gmail.com>
2020-09-02 11:20:47 -0400

    add base route

--- app.rb
+++ app.rb
@@ -4,3 +4,13 @@ require './config'
 get '/' do
     'hola posible bebedor, bienvenido a al juego Cronograma, busca la palabra con mayor puntaje, y si pierdes debes beber'
 end
+
+get '/game/' do
+    erb :hello_form
+end
+
+post '/game/' do
+    result = params[:word] || ""
+
+    erb :index, :locals => {'result' => result}
+end
